aa65e637654873cb67f40af62cd0bf9d4419c411
alexmedve <41807747+alexmedve@users.noreply.github.com>
2020-12-07 20:47:33 +0200

    minimized images and css

---
+++ js/home.js
@@ -0,0 +1,10 @@
+var test = new hoverEffect({
+    parent: document.querySelector('.home__col--1'),
+    intensity: 0.3,
+    image1: '../assets/img/home-1.jpeg',
+    image2: '../assets/img/home-2.jpeg',
+    displacementImage: '../assets/img/distorsion2.png'
+});
+
+document.querySelector('canvas').style.removeProperty('width');
+document.querySelector('canvas').style.removeProperty('height');
